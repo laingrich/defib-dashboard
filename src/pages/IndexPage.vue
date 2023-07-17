e3a8496a452012f396070e9d0da229508051fdbf
richard laing <114079626+richlaing@users.noreply.github.com>
2023-07-17 11:39:54 +0100

    feat(Controls): Deploys and events overlaid map as control

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,82 +1,85 @@
 <template>
   <q-page>
-    <div style="height:600px; width:800px">
-    <l-map ref="map" :zoom="zoom" :center="mapCenter" :options="{zoomControl: false, dragging: false, boxZoom: false, scrollWheelZoom: false}">
-      <l-tile-layer
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-        layer-type="base"
-        name="OpenStreetMap"
-      ></l-tile-layer>
-      <l-marker
-        v-for="cabinet in cabinets"
-        :key="cabinet.id"
-        :lat-lng="cabinet.location"></l-marker>
-    </l-map>
-  </div>
-    <div class="row justify-start">
-      <div class="q-pa-md"></div>
-      <!-- Deployments -->
-      <div class="q-px-xl q-pt-lg">
-        <h6 class="q-my-sm">Latest Deployments</h6>
-        <q-timeline class="text q-mx-lg q-pt-lg">
-          <q-timeline-entry
-            v-for="deployment in deployments"
-            :key="deployment.id"
-            :color="deployment.color"
-            :title="deployment.name"
-            :subtitle="deployment.date"
-          >
-            <div>
-              {{ deployment.status }}
+    <div style="height:1030px; width:1920px">
+      <l-map ref="map" :zoom="zoom" :center="mapCenter" :options="{zoomControl: false, dragging: false, boxZoom: false, scrollWheelZoom: false}">
+        <l-tile-layer
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+        ></l-tile-layer>
+        <l-marker
+          v-for="cabinet in cabinets"
+          :key="cabinet.id"
+          :lat-lng="cabinet.location">
+        </l-marker>
+        <l-control position="topleft" >
+          <div class="row justify-start">
+            <div class="q-pa-md"></div>
+            <!-- Deployments -->
+            <div class="q-px-xl q-pt-lg">
+              <h6 class="q-my-sm">Latest Deployments</h6>
+              <q-timeline class="text q-mx-lg q-pt-lg">
+                <q-timeline-entry
+                  v-for="deployment in deployments"
+                  :key="deployment.id"
+                  :color="deployment.color"
+                  :title="deployment.name"
+                  :subtitle="deployment.date"
+                >
+                  <div>
+                    {{ deployment.status }}
+                  </div>
+                </q-timeline-entry>
+              </q-timeline>
             </div>
-          </q-timeline-entry>
-        </q-timeline>
-      </div>
-      <!-- Events -->
-      <div id="events" class="q-pt-lg q-pb-xl q-px-xl" style="width: 350px">
-        <h6 class="q-my-sm">Latest Events</h6>
-        <q-list class="text">
-          <transition-group name="list" tag="div">
-            <q-item
-              v-for="event in displayedEvents"
-              :key="event.id"
-              class="list-item"
-            >
-              <q-item-section avatar>
-                <q-icon :color="event.iconColor" :name="event.icon" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>{{ event.msg }}</q-item-label>
-                <q-item-label caption>{{ event.timestamp }}</q-item-label>
-              </q-item-section>
-            </q-item>
-          </transition-group>
-        </q-list>
-      </div>
-      <!-- Totals-->
-      <div class="q-pt-xl q-px-xl" style="max-width: 350px">
-        <q-list>
-          <q-item
-            class="q-chip row inline no-wrap items-center text-green q-chip--colored q-chip--outline q-chip--clickable cursor-pointer non-selectable q-hoverable q-chip--square"
-            tabindex="0"
-            style="font-size: 24px"
-          >
-            <div
-              class="q-chip__content col row no-wrap items-center q-anchor--skip"
-            >
-              <div class="q-avatar bg-green text-white" style="width: 1.5em">
-                <div class="q-avatar__content row flex-center overflow-hidden">
-                  {{ cabinets.length }}
-                </div>
-              </div>
-              <div
-                class="q-item__section column q-item__section--main justify-center q-py-md"
-              ></div>
-              <div class="text-subtitle2">Total Defibs</div>
+            <!-- Events -->
+            <div id="events" class="q-pt-lg q-pb-xl q-px-xl" style="width: 350px">
+              <h6 class="q-my-sm">Latest Events</h6>
+              <q-list class="text">
+                <transition-group name="list" tag="div">
+                  <q-item
+                    v-for="event in displayedEvents"
+                    :key="event.id"
+                    class="list-item"
+                  >
+                    <q-item-section avatar>
+                      <q-icon :color="event.iconColor" :name="event.icon" />
+                    </q-item-section>
+                    <q-item-section>
+                      <q-item-label>{{ event.msg }}</q-item-label>
+                      <q-item-label caption>{{ event.timestamp }}</q-item-label>
+                    </q-item-section>
+                  </q-item>
+                </transition-group>
+              </q-list>
             </div>
-          </q-item>
-        </q-list>
-      </div>
+            <!-- Totals-->
+            <div class="q-pt-xl q-px-xl" style="max-width: 350px">
+              <q-list>
+                <q-item
+                  class="q-chip row inline no-wrap items-center text-green q-chip--colored q-chip--outline q-chip--clickable cursor-pointer non-selectable q-hoverable q-chip--square"
+                  tabindex="0"
+                  style="font-size: 24px"
+                >
+                  <div
+                    class="q-chip__content col row no-wrap items-center q-anchor--skip"
+                  >
+                    <div class="q-avatar bg-green text-white" style="width: 1.5em">
+                      <div class="q-avatar__content row flex-center overflow-hidden">
+                        {{ cabinets.length }}
+                      </div>
+                    </div>
+                    <div
+                      class="q-item__section column q-item__section--main justify-center q-py-md"
+                    ></div>
+                    <div class="text-subtitle2">Total Defibs</div>
+                  </div>
+                </q-item>
+              </q-list>
+            </div>
+          </div>
+        </l-control>
+      </l-map>
     </div>
   </q-page>
 </template>
@@ -84,7 +87,7 @@
 <script>
 import { defineComponent } from 'vue'
 import 'leaflet/dist/leaflet.css'
-import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet'
+import { LMap, LTileLayer, LMarker, LControl } from '@vue-leaflet/vue-leaflet'
 import { latLng } from 'leaflet'
 // delete this line
 import { cabinetStream, deploymentStream } from '../boot/firebase'
@@ -96,13 +99,14 @@ export default defineComponent({
   components: {
     LMap,
     LTileLayer,
-    LMarker
+    LMarker,
+    LControl
   },
 
   data () {
     return {
-      zoom: 8,
-      mapCenter: latLng(50.263, -5.051),
+      zoom: 9,
+      mapCenter: latLng(50.6, -5),
       deployments: [],
       events: [
         {
