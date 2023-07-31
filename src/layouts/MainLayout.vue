b90568a6fd97aac5cd4efcfbcc064f0e81575fb6
richard laing <114079626+richlaing@users.noreply.github.com>
2023-07-31 15:04:16 +0100

    fix(Main layout - fix typo):

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,7 +6,7 @@
         <q-toolbar-title>
           Defib Dashboard - live updates across Cornwall of defibs monitored by Defib-Ready.com
         </q-toolbar-title>
-        <span class="q-pr-lg">Bought to you by Tango3 and Duchy Defibrillators</span>
+        <span class="q-pr-lg">Brought to you by Tango3 and Duchy Defibrillators</span>
       </q-toolbar>
     </q-header>
 
